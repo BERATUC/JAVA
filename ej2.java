c63a00e510109ad3eb8249ecb3da2d2829426f1a
BERA <beratucuman@gmail.com>
2023-03-29 21:08:03 -0300

    mensaje confirm

---
+++ ej2.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintro01;
+import java.util.Scanner;
+
+import java.lang.System;
+
+/**
+ * 
+ 
+ * @author vs
+ */
+public class ej2 {
+    
+    
+    
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        String nombre;
+        System.out.print("Introduzca su nombre: ");
+        nombre = lectura.nextLine(); //asigna a la variable n el número entero introducido por teclado  
+// TODO code application logic here
+        System.out.println(nombre);
+        
+        
+        System.out.println(nombre.toUpperCase());     
+        limp();
+        
+        
+        
+    }
+    public static void limp(String[] args) {
+        // Limpiar la consola
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }
+}
